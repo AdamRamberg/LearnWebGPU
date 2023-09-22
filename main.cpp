348dfe45d4206a224f1366b96826de3aaa148b07
Adam Ramberg <adam@mambojambostudios.com>
2023-09-22 14:02:45 +0200

    Add color attributes to vertex shader

--- main.cpp
+++ main.cpp
@@ -58,17 +58,18 @@ int main (int, char**) {
     // Create required limits
     RequiredLimits requiredLimits = Default;
 	// We use at most 1 vertex attribute for now
-	requiredLimits.limits.maxVertexAttributes = 1;
+	requiredLimits.limits.maxVertexAttributes = 2;
 	// We should also tell that we use 1 vertex buffers
 	requiredLimits.limits.maxVertexBuffers = 1;
 	// Maximum size of a buffer is 6 vertices of 2 float each
-	requiredLimits.limits.maxBufferSize = 6 * 2 * sizeof(float);
+	requiredLimits.limits.maxBufferSize = 6 * 5 * sizeof(float);
 	// Maximum stride between 2 consecutive vertices in the vertex buffer
-	requiredLimits.limits.maxVertexBufferArrayStride = 2 * sizeof(float);
+	requiredLimits.limits.maxVertexBufferArrayStride = 5 * sizeof(float);
 	// This must be set even if we do not use storage buffers for now
 	requiredLimits.limits.minStorageBufferOffsetAlignment = supportedLimits.limits.minStorageBufferOffsetAlignment;
 	// This must be set even if we do not use uniform buffers for now
 	requiredLimits.limits.minUniformBufferOffsetAlignment = supportedLimits.limits.minUniformBufferOffsetAlignment;
+    requiredLimits.limits.maxInterStageShaderComponents = 3;
 
     // Setup device
     DeviceDescriptor deviceDesc{};
@@ -118,15 +119,28 @@ int main (int, char**) {
 
 	std::cout << "🚚 Creating shader module..." << std::endl;
 	const char* shaderSource = R"(
+struct VertexInput {
+    @location(0) position: vec2f,
+    @location(1) color: vec3f,
+}
+
+struct VertexOutput {
+    @builtin(position) position: vec4f,
+    @location(0) color: vec3f,
+}
+
 @vertex
-fn vs_main(@location(0) in_vertex_position: vec2f) -> @builtin(position) vec4f {
-	return vec4f(in_vertex_position, 0.0, 1.0);
+fn vs_main(in: VertexInput) -> VertexOutput {
+    var out: VertexOutput;
+    out.position = vec4f(in.position, 0.0, 1.0);
+    out.color = in.color;
+	return out;
 }
 
 
 @fragment
-fn fs_main() -> @location(0) vec4f {
-    return vec4f(0.0, 0.4, 1.0, 1.0);
+fn fs_main(in: VertexOutput) -> @location(0) vec4f {
+    return vec4f(in.color, 1.0);
 }
 )";
     // Setup shader module
@@ -151,21 +165,23 @@ fn fs_main() -> @location(0) vec4f {
 
 	std::cout << "🚚 Creating render pipeline..." << std::endl;
 	// Vertex fetch
-	VertexAttribute vertexAttrib;
-	// == Per attribute ==
-	// Corresponds to @location(...)
-	vertexAttrib.shaderLocation = 0;
-	// Means vec2<f32> in the shader
-	vertexAttrib.format = VertexFormat::Float32x2;
-	// Index of the first element
-	vertexAttrib.offset = 0;
+	std::vector<VertexAttribute> vertexAttribs(2);
+	// Position attribute
+    vertexAttribs[0].shaderLocation = 0;
+    vertexAttribs[0].format = VertexFormat::Float32x2;
+    vertexAttribs[0].offset = 0;
+
+    // Color attribute
+    vertexAttribs[1].shaderLocation = 1;
+    vertexAttribs[1].format = VertexFormat::Float32x3; // different type!
+    vertexAttribs[1].offset = 2 * sizeof(float); // non null offset!
 
 	VertexBufferLayout vertexBufferLayout;
 	// [...] Build vertex buffer layout
-	vertexBufferLayout.attributeCount = 1;
-	vertexBufferLayout.attributes = &vertexAttrib;
+    vertexBufferLayout.attributeCount = (uint32_t)vertexAttribs.size();
+	vertexBufferLayout.attributes = vertexAttribs.data();
 	// == Common to attributes from the same buffer ==
-	vertexBufferLayout.arrayStride = 2 * sizeof(float);
+	vertexBufferLayout.arrayStride = 5 * sizeof(float);
 	vertexBufferLayout.stepMode = VertexStepMode::Vertex;
 
     // Setup render pipeline
@@ -233,15 +249,17 @@ fn fs_main() -> @location(0) vec4f {
 
     // Static vertex buffer
     std::vector<float> vertexData = {
-        -0.5, -0.5,
-        +0.5, -0.5,
-        +0.0, +0.5,
+        // x0,  y0,  r0,  g0,  b0
+        -0.5, -0.5, 1.0, 0.0, 0.0,
+        // x1,  y1,  r1,  g1,  b1
+        +0.5, -0.5, 0.0, 1.0, 0.0,
+        +0.0,   +0.5, 0.0, 0.0, 1.0,
 
-        -0.55f, -0.5,
-        -0.05f, +0.5,
-        -0.55f, +0.5,
+        -0.55f, -0.5, 1.0, 1.0, 0.0,
+        -0.05f, +0.5, 1.0, 0.0, 1.0,
+        -0.55f, +0.5, 0.0, 1.0, 1.0
     };
-    int vertexCount = static_cast<int>(vertexData.size() / 2);
+    int vertexCount = static_cast<int>(vertexData.size() / 5);
 
     // Upload geometry to the GPU via buffer
 	BufferDescriptor bufferDesc;
@@ -280,7 +298,7 @@ fn fs_main() -> @location(0) vec4f {
         renderPassColorAttachment.resolveTarget = nullptr;
         renderPassColorAttachment.loadOp = LoadOp::Clear;
         renderPassColorAttachment.storeOp = StoreOp::Store;
-        renderPassColorAttachment.clearValue = Color{ 0.9, 0.1, 0.2, 1.0 };
+        renderPassColorAttachment.clearValue = Color{ 0.1, 0.1, 0.1, 1.0 };
         renderPassDesc.colorAttachmentCount = 1;
         renderPassDesc.colorAttachments = &renderPassColorAttachment;
         // No depth buffer for now
